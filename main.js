9954e5bb34f9f5ad3f191af640e75c239f0d3f1e
meng-Macbook <1572482184@qq.com>
2019-05-16 23:53:51 +0800

    '123'

--- main.js
+++ main.js
@@ -2,22 +2,24 @@
 
 (function () {
 
-    let queue = []
-    let tips = []
+    // 初始化状态
+    const STATE_INITAL = 0
+    // 开始状态
+    const STATE_START = 1
 
-    let cid = 0
+    let queue = []
 
     let _config = {
         title: '',
         content: '',
-        duration: 10000,
+        duration: 1000,
         direction: 'top',
         shade: true,
         multile: false,
         onComplete: function () {}
     }
 
-    let instance = undefined
+    let instance
 
 
     /**
@@ -27,27 +29,28 @@
      */
     function createTip(config) {
 
-        // 判断是否有实例化的对象
-        if (instance !== undefined) {
+        if (instance instanceof Tip) {
             return instance.init(config)
-        }
+        }     
 
-        instance = new Tip(config)     
+        instance = new Tip(config)
         return instance
     }
 
     function Tip(config) {    
+        this.cid = 0
+        this.index = 0
+        this.list = []
+        this.timer = undefined
+        this.wrapper = undefined
+        this.state = STATE_INITAL
         this.init(config)
+        return this
     }
 
     Tip.prototype.init = function (config) {
 
-        if (this.wrapper && !this.config.multile) {
-            this.wrapper.remove()
-        }
-
-        this.config = Object.create(_config)
-
+        // 如果是数字就转换成字符串
         if (isTypeof(config) === 'number') {
             config = config.toString()
         }
@@ -57,22 +60,38 @@
             throw new Error('请输入字符串或者对象')
         }
 
+        // 如果不是初始化，就还原
+        if (this.state === STATE_START) {
+            this.destroy(config) 
+        }
+
+        this.state = STATE_START
+
+        this.config = Object.create(_config)
         isTypeof(config) === 'string' ? (this.config.content = config) : Object.assign(this.config, config)
-        this.cid = config.multile ? cid++ : cid
+        this.index++
 
         this.render()
+        return this
     }
 
     Tip.prototype.render = function () {
 
+        if (this.state !== STATE_START) {
+            return this
+        }
+
+        let that = this
+
         this.wrapper = document.createElement('div')
         this.wrapper.className = 'ui-tip-wrapper'
-        this.wrapper.id = 'ui-tip-wrapper-' + this.cid
+        this.wrapper.id = 'ui-tip-wrapper-' + this.index
         document.body.append(this.wrapper)  
+        queue.push(this)
 
-        tips.push(this.wrapper)
         this.wrapper.innerHTML = this.view()
         this.animation()
+        return this
     }
 
     Tip.prototype.view = function () {
@@ -95,31 +114,53 @@
         addCss(this.wrapper, {
             top: "50%",
             left: "50%",
-            transform: 'translate(-50%, -50%) scale(0)',
+            opacity: 0,
+            transform: 'translate(-50%, 50%)',
         })
 
-        TweenMax.to(this.wrapper, 0.2, {
+        this.tween = TweenMax.to(this.wrapper, 0.5, {
             opacity: 1,
-            transform: 'translate(-50%, -50%) scale(1)',
+            transform: 'translate(-50%, -50%)',
             onComplete: function () {
                 that.timer = setTimeout(function () {
-                    animationOut(that)
-                    that.config.onComplete.call(that)
-                    that.destroy()
+                    that.tween.reverse()
                 }, that.config.duration)
+            },
+            onReverseComplete: function () {
+                that.config.onComplete.call(that)
+                that.destroy()
             }
         })
 
-        function animationOut(ts) {
-            TweenMax.to(ts.wrapper, 0.1, {
-                opacity: 0,
-                transform: 'translate(-50%, -200%)',
-            })            
-        }
     }
 
     Tip.prototype.destroy = function () {
-        this.wrapper.remove()
+        if (this.state === STATE_INITAL) {
+            return this
+        }
+
+        this.state = STATE_INITAL
+
+        // 如果有动画操作
+        if (this.tween) {
+            this.tween.kill()
+            this.tween = undefined
+        }
+
+        // 如果有 wrapper
+        if (this.wrapper) {
+            this.wrapper.remove()
+            this.wrapper = undefined
+        }
+
+        // 如果有时间控制函数
+        if (this.timer) {
+            clearTimeout(this.timer)
+            this.timer = undefined
+        }
+
+
+        // queue[this.index] = undefined
     }
 
     function addCss(el, propertys) {
@@ -138,156 +179,6 @@
     }
 
 
-
-
-    // tip.view = function (year, month) {
-    //     monthData = datepicker.getMonthData(year, month)
-    //     let _ = queue[activeIndex] || {}
-    //     let html = `
-    //     <div class="ui-datepicker-header">
-    //         <div data-direction="prev" class="ui-datepicker-btn ui-datepicker-prev-btn">&lt;</div>
-    //         <div data-direction="next" class="ui-datepicker-btn ui-datepicker-next-btn">&gt;</div>
-    //         ${monthData.year}-${monthData.month}
-    //     </div>
-    //     <div class="ui-datepicker-body">
-    //         <table>
-    //             <thead>
-    //                 <tr>
-    //                     <th>一</th>
-    //                     <th>二</th>
-    //                     <th>三</th>
-    //                     <th>四</th>
-    //                     <th>五</th>
-    //                     <th>六</th>
-    //                     <th>日</th>
-    //                 </tr>
-    //             </thead>
-    //             <tbody>
-    //                 <tr>
-    //                     ${monthData.days.map((item, i) => `${i % 7 === 0 ? '<tr>' : ''}<td date="${item.time}" class="${item.month !== monthData.month ? 'other-month' : ''} ${item.time === _.activeTime ? 'active' : ''}">${item.showDate}</td>${i % 7 === 6 ? '</tr>' : ''}`).join('')}
-    //                 </tr>
-    //             </tbody>
-    //         </table>
-    //     </div>
-    //     `
-    //     return html
-    // }
-
-    // tip.render = function (direction) {
-    //     if (monthData) {
-    //         year = monthData.year
-    //         month = monthData.month
-    //     } else {
-    //         let today = new Date()
-    //         year = today.getFullYear()
-    //         month = today.getMonth() + 1
-    //     }
-
-    //     if (direction === 'prev') month--
-    //     if (direction === 'next') month++
-
-    //     let _ = queue[activeIndex]
-
-    //     let $wrapper = _ ? _.wrapper :  document.querySelector('.ui-datepicker-wrapper' + activeIndex)
-
-    //     if (!$wrapper) {
-    //         $wrapper = document.createElement('div')
-    //         $wrapper.className = 'ui-datepicker-wrapper ui-datepicker-wrapper' + activeIndex
-    //         document.body.append($wrapper)
-    //     }
-
-    //     $wrapper.innerHTML = datepicker.view(year, month)
-
-    //     return $wrapper
-    // }
-
-    // tip.init = function (input) {
-
-    //     queue.push({
-    //         input: document.querySelector(input),
-    //         wrapper: datepicker.render(),
-    //         isOpen: false,
-    //         index: index,
-    //         activeTime: "",
-    //         value: '',
-    //     })
-
-    //     let _ = queue[activeIndex]
-    //     activeIndex = ++index
-
-    //     // 当输入框点击的时候
-    //     _.input.addEventListener('click', function (e) {
-
-    //         activeIndex = _.index
-
-    //         // 清除之前的
-    //         for(let item of queue) {
-    //             if (item.isOpen) {
-    //                 item.isOpen = false
-    //                 item.wrapper.classList.remove('ui-datepicker-wrapper-show')
-    //             }
-    //         }
-
-    //         _.isOpen = !_.isOpen
-
-    //         if (_.isOpen) {
-    //             _.wrapper.classList.add('ui-datepicker-wrapper-show')
-
-    //             let top = this.offsetTop
-    //             let left = this.offsetLeft
-    //             let height = this.offsetHeight
-
-    //             _.wrapper.style.top = top + height + 2 + 'px'
-    //             _.wrapper.style.left = left + 'px'
-
-    //             let inputMonth = _.value.split('-')[1]
-
-    //             if (inputMonth > month) {
-    //                 datepicker.render('next')
-    //             }
-
-    //             if (inputMonth < month) {
-    //                 datepicker.render('prev')
-    //             }
-
-    //         } else {
-    //             _.wrapper.classList.remove('ui-datepicker-wrapper-show')
-    //         }
-
-
-    //     }, false)
-
-    //     _.wrapper.addEventListener('click', function (e) {
-
-    //         activeIndex = _.index
-
-    //         let $target = e.target
-
-    //         if ($target.tagName.toLowerCase() === 'td') {
-    //             _.value = _.activeTime = $target.getAttribute('date')
-    //             _.input.value = format(_.value)
-    //             _.isOpen = false
-    //             _.wrapper.classList.remove('ui-datepicker-wrapper-show')
-    //             datepicker.render()
-    //         }
-
-    //         // 点击月份切换按钮
-    //         if ($target.classList.contains('ui-datepicker-btn')) {
-    //             let direction = $target.dataset.direction
-    //             datepicker.render(direction)
-    //         }
-
-    //         function format(value) {
-    //             let date = value.split('-')
-    //             for(let i = 0; i < date.length; i++) {
-    //                 let item = date[i]
-    //                 if (item < 9) {  date[i] = "0" + item }
-    //             }
-
-    //             return date.join('-')
-    //         }
-    //     }, false)
-    // }
 
 
     window.tip = createTip
